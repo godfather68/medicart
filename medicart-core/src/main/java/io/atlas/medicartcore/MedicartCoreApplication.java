7498ff2bde801b097aa21026ce25f6d87f55cadc
FarelEllely <edy.ellely@gmail.com>
2017-11-07 11:58:25 +0530

    medicart-core app

---
+++ medicart-core/src/main/java/io/atlas/medicartcore/MedicartCoreApplication.java
@@ -0,0 +1,12 @@
+package io.atlas.medicartcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicartCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicartCoreApplication.class, args);
+	}
+}

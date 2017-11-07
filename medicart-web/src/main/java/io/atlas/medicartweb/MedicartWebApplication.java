cb463dad71e3a7bb611561eaab46ecbc24d3245b
FarelEllely <edy.ellely@gmail.com>
2017-11-07 22:26:47 +0530

    medicart-web app
    Signed-off-by: FarelEllely <edy.ellely@gmail.com>

---
+++ medicart-web/src/main/java/io/atlas/medicartweb/MedicartWebApplication.java
@@ -0,0 +1,12 @@
+package io.atlas.medicartweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicartWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicartWebApplication.class, args);
+	}
+}
